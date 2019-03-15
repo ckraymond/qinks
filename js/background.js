144970e7f1340c79390f1df6cbe2d14bb2a07a69
ckraymond <colin.k.raymond@gmail.com>
2019-03-14 21:54:49 -0400

    Making another change and switching computers.

--- js/background.js
+++ js/background.js
@@ -1,3 +1,19 @@
 // Go Links
 // Background scripts
 // Author: Colin Raymond (colin.k.raymond@gmail.com)
+
+function updateSheet(text) {
+  console.log("Access sheet: " + text);
+};
+
+chrome.omnibox.onInputEntered.addListener(
+  function(text) {
+    switch(text.substring(0,3)) {
+      case 'st/':
+        updateSheet(text.substring(3));
+        break;
+      default:
+        console.log(text);
+    }
+  }
+);
